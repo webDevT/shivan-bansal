4b711ba40d2eaa1a74958a8726d2a71485b1672b
Karina <karinadesyatnik27@gmail.com>
2023-09-26 19:02:37 +0300

    all

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,43 @@ $(function(){
         $(this).toggleClass('active');
         $('.menu').slideToggle();
     });
+  
 
+$('.view-all-button').click(function(){
+    $(this).hide();
+    $('.blog').addClass('active');
+})
+
+$('.project-card .button').click(function() {
+    $(this).parent().parent().find('.project-description-modal').fadeIn();
+})
+
+$('.close-btn').click(function(){
+    $('.project-description-modal').fadeOut();
+})
+
+$(".menu").on("click","a", function (event) {
+
+   
+    event.preventDefault();
+
+
+
+    var id  = $(this).attr('href'),
+
+
+
+    top = $(id).offset().top;
+
+
+    $('body,html').animate({scrollTop: top}, 1500);
 
 });
+
+});
+if($(window).width() < 768) {
+$('.menu__link').click(function(){
+    $('.menu').slideUp();
+    $('.menu-btn').removeClass('active');
+})
+}
